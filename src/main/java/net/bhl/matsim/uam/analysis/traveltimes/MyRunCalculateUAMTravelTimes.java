af21fa6700d18940bfd0394771cf3efe02576881
Hao Wu <56875899+haowuintub@users.noreply.github.com>
2023-02-24 13:48:27 +0100

    Bug fixed!

--- src/main/java/net/bhl/matsim/uam/analysis/traveltimes/MyRunCalculateUAMTravelTimes.java
+++ src/main/java/net/bhl/matsim/uam/analysis/traveltimes/MyRunCalculateUAMTravelTimes.java
@@ -1,18 +1,22 @@
 package net.bhl.matsim.uam.analysis.traveltimes;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.*;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import com.opencsv.CSVParser;
 import net.bhl.matsim.uam.analysis.traveltimes.traveldisutility.NoiseBasedTravelDisutility;
+import net.bhl.matsim.uam.data.UAMRoute;
+import net.bhl.matsim.uam.router.strategy.*;
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
 import org.matsim.core.config.Config;
@@ -22,10 +26,7 @@ import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Injector;
 import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.network.algorithms.TransportModeNetworkFilter;
-import org.matsim.core.router.AStarLandmarksFactory;
-import org.matsim.core.router.DijkstraFactory;
-import org.matsim.core.router.RoutingModule;
-import org.matsim.core.router.TeleportationRoutingModule;
+import org.matsim.core.router.*;
 import org.matsim.core.router.util.LeastCostPathCalculator;
 import org.matsim.core.router.util.LeastCostPathCalculatorFactory;
 import org.matsim.core.router.util.TravelDisutility;
@@ -33,6 +34,7 @@ import org.matsim.core.router.util.TravelDisutilityUtils;
 import org.matsim.core.router.util.TravelTime;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.trafficmonitoring.TravelTimeCalculator;
+import org.matsim.facilities.Facility;
 import org.matsim.pt.router.TransitRouter;
 import org.matsim.utils.MemoryObserver;
 
@@ -63,7 +65,7 @@ import net.bhl.matsim.uam.run.UAMConstants;
 
 public class MyRunCalculateUAMTravelTimes {
     private static final int processes = Runtime.getRuntime().availableProcessors();
-    private static final Logger log = Logger.getLogger(RunCalculateUAMTravelTimes.class);
+    private static final Logger log = Logger.getLogger(MyRunCalculateUAMTravelTimes.class);
     private static ArrayBlockingQueue<LeastCostPathCalculator> carRouters = new ArrayBlockingQueue<>(processes);
     private static ArrayBlockingQueue<TransitRouter> ptRouters = new ArrayBlockingQueue<>(processes);
     private static ArrayBlockingQueue<LeastCostPathCalculator> uamRouters = new ArrayBlockingQueue<>(processes);
@@ -177,7 +179,7 @@ public class MyRunCalculateUAMTravelTimes {
             while (threadCounter.getProcesses() >= processes - 1)
                 Thread.sleep(200);
 
-            es.execute(new RunCalculateUAMTravelTimes.UAMTravelTimeCalculator(threadCounter, network, config, trip, uamManager, networkCar,
+            es.execute(new UAMTravelTimeCalculator(threadCounter, network, config, trip, uamManager, networkCar,
                     scenario, stationConnectionutilities));
             counter++;
         }
@@ -188,9 +190,139 @@ public class MyRunCalculateUAMTravelTimes {
 
         // Writes output file
         log.info("Writing travel times file...");
-        RunCalculateUAMTravelTimes.write(outputPath, trips);
+        write(outputPath, trips);
         log.info("...done.");
 
         MemoryObserver.stop();
     }
+
+    // --- unchanged copy & paste
+    public static void write(String outputPath, List<TripItem> trips) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputPath)));
+
+        writer.write(formatHeader() + "\n");
+        for (TripItem trip : trips) {
+            writer.write(String.join(String.valueOf(CSVParser.DEFAULT_SEPARATOR),
+                    new String[] { String.valueOf(trip.origin.getX()), String.valueOf(trip.origin.getY()),
+                            String.valueOf(trip.destination.getX()), String.valueOf(trip.destination.getY()),
+                            String.valueOf(trip.departureTime), String.valueOf(trip.travelTime),
+                            String.valueOf(trip.accessTime), String.valueOf(trip.flightTime),
+                            String.valueOf(trip.egressTime), trip.accessMode, trip.egressMode, trip.originStation,
+                            trip.destinationStation })
+                    + "\n");
+        }
+
+        writer.flush();
+        writer.close();
+    }
+
+    private static String formatHeader() {
+        return String.join(String.valueOf(CSVParser.DEFAULT_SEPARATOR),
+                new String[] { "origin_x", "origin_y", "destination_x", "destination_y", "departure_time",
+                        "travel_time", "access_time", "flight_time", "egress_time", "access_mode", "egress_mode",
+                        "orig_station", "dest_station" });
+    }
+
+    static class UAMTravelTimeCalculator implements Runnable {
+
+        private TripItem trip;
+        private Network network;
+        private Config config;
+        private ThreadCounter threadCounter;
+        private UAMManager uamManager;
+        private Network networkCar;
+        private Scenario scenario;
+        private UAMStationConnectionGraph stationConnectionutilities;
+        private LeastCostPathCalculator pathCalculator;
+        private LeastCostPathCalculator plcpccar;
+        private TransitRouter transitRouter;
+
+        UAMTravelTimeCalculator(ThreadCounter threadCounter, Network network, Config config, TripItem trip,
+                                UAMManager uamManager, Network networkCar, Scenario scenario,
+                                UAMStationConnectionGraph stationConnectionutilities) {
+            this.trip = trip;
+            this.network = network;
+            this.config = config;
+            this.threadCounter = threadCounter;
+            this.uamManager = uamManager;
+            this.networkCar = networkCar;
+            this.scenario = scenario;
+            this.stationConnectionutilities = stationConnectionutilities;
+        }
+
+        @Override
+        public void run() {
+            threadCounter.register();
+
+            try {
+                pathCalculator = uamRouters.take();
+                plcpccar = carRouters.take();
+                transitRouter = ptRouters.take();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Link from = NetworkUtils.getNearestLink(network, trip.origin);
+            Link to = NetworkUtils.getNearestLink(network, trip.destination);
+            Facility fromFacility = new LinkWrapperFacility(from);
+            Facility toFacility = new LinkWrapperFacility(to);
+
+            UAMConfigGroup uamConfig = (UAMConfigGroup) config.getModules().get(UAMConstants.uam);
+            UAMStrategyUtils strategyUtils = new UAMStrategyUtils(uamManager.getStations(), uamConfig, scenario,
+                    stationConnectionutilities, networkCar, transitRouter, plcpccar);
+            UAMStrategy strategy = null;
+            switch (uamConfig.getRoutingStrategy()) {
+                case MINTRAVELTIME:
+                    strategy = new UAMMinTravelTimeStrategy(strategyUtils);
+                    break;
+                case MINACCESSTRAVELTIME:
+                    strategy = new UAMMinAccessTravelTimeStrategy(strategyUtils);
+                    break;
+                case MINDISTANCE:
+                    strategy = new UAMMinDistanceStrategy(strategyUtils);
+                    break;
+                case MINACCESSDISTANCE:
+                    strategy = new UAMMinAccessDistanceStrategy(strategyUtils);
+                    break;
+                default:
+                    log.warn("Strategy not available, please provide an available strategy.");
+                    System.exit(-1);
+            }
+
+            Optional<UAMRoute> optionalUamRoute = strategy.getRoute(null, fromFacility, toFacility, trip.departureTime);
+
+            if (optionalUamRoute.isPresent()) {
+                UAMRoute uamRoute = optionalUamRoute.get();
+
+                trip.accessMode = uamRoute.accessMode;
+                trip.egressMode = uamRoute.egressMode;
+                trip.originStation = uamRoute.bestOriginStation.getId().toString();
+                trip.destinationStation = uamRoute.bestDestinationStation.getId().toString();
+
+                trip.processTime = uamRoute.bestOriginStation.getPreFlightTime()
+                        + uamRoute.bestDestinationStation.getPostFlightTime();
+
+                trip.accessTime = strategyUtils.getAccessTime(fromFacility, trip.departureTime,
+                        uamRoute.bestOriginStation, uamRoute.accessMode);
+
+                trip.flightTime = strategyUtils.getFlightTime(uamRoute.bestOriginStation,
+                        uamRoute.bestDestinationStation);
+
+                trip.egressTime = strategyUtils.getEgressTime(toFacility, trip.departureTime,
+                        uamRoute.bestDestinationStation, uamRoute.egressMode);
+
+                trip.travelTime = trip.accessTime + trip.flightTime + trip.egressTime + trip.processTime;
+            }
+
+            try {
+                ptRouters.put(transitRouter);
+                uamRouters.put(pathCalculator);
+                carRouters.put(plcpccar);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            threadCounter.deregister();
+        }
+    }
+
 }
