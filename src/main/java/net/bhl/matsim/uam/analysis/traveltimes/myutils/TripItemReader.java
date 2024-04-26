3f2e0e15c5e4fc49d7efbdf5a05c2c8b0de30242
Hao Wu <wu.hao@tum.de>
2024-04-26 13:40:53 +0200

    copy&paste

---
+++ src/main/java/net/bhl/matsim/uam/analysis/traveltimes/myutils/TripItemReader.java
@@ -0,0 +1,25 @@
+package net.bhl.matsim.uam.analysis.traveltimes.myutils;
+
+import org.matsim.api.core.v01.Coord;
+import org.matsim.contrib.util.CSVReaders;
+import org.matsim.core.utils.misc.Time;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TripItemReader {
+    public static List<TripItem> getTripItems(String tripsInput) throws IOException {
+        List<TripItem> trips = new ArrayList<>();
+        List<String[]> rows = CSVReaders.readCSV(tripsInput);
+        for (String[] row : rows.subList(1, rows.size())) {
+            int j = 0;
+            TripItem trip = new TripItem();
+            trip.origin = new Coord(Double.parseDouble(row[j++]), Double.parseDouble(row[j++]));
+            trip.destination = new Coord(Double.parseDouble(row[j++]), Double.parseDouble(row[j++]));
+            trip.departureTime = Time.parseTime(row[j]);
+            trips.add(trip);
+        }
+        return trips;
+    }
+}
