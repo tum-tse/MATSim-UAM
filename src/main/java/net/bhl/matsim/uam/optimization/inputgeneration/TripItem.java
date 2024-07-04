a58341aab29abe182114e38333cf6b43965d7074
shahriarzame <68831730+shahriarzame@users.noreply.github.com>
2024-07-04 12:35:44 +0200

    Made the Index Static, added TripID, conversion of "departure_time" from minutes to seconds.

---
+++ src/main/java/net/bhl/matsim/uam/optimization/inputgeneration/TripItem.java
@@ -0,0 +1,23 @@
+package net.bhl.matsim.uam.optimization.inputgeneration;
+
+import org.matsim.api.core.v01.Coord;
+
+
+public class TripItem {
+    public Coord origin;
+    public Coord destination;
+    public double departureTime;
+    public double travelTime;
+    public String description;
+
+    public double distance;
+    public double accessTime;
+    public double flightTime;
+    public double egressTime;
+    public double processTime;
+    public String accessMode;
+    public String egressMode;
+    public String originStation;
+    public String destinationStation;
+    public String tripID;
+}
